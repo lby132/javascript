e0c18195330855a834fb2108d72a49f7733df3f2
lby132 <quddbscjswow@naver.com>
2022-10-07 01:17:15 +0900

    문제풀이

--- war.js
+++ war.js
@@ -1,63 +1,14 @@
+const prop = {
 
-// const array = ['🍌', '🍓', '🍇', '🍓'];
-// const arr = Array.from(array);
-// array.forEach((el, num) => {
-//     if(arr[from] === '🍓') {
-//         arr[from] = '🥝';
-//     }
-// });
-// console.log(arr);
+    name1: 'Button',
+    styles: {
+      size: 20,
+      color: 'black',
+    },
+  };
 
-// const count = ['🍌', '🥝', '🍇', '🥝'];
-// let cn = 0;
-// count.forEach(function(a){
-//     if (a === '🥝') cn++;
-// });
-// console.log(cn);
+function changeColor({ styles:{color}}) {
+    console.log(color);
+}
 
-
-// function match(input, search) {
-//     const result = [];
-//     for (let i = 0; i < input.length; i++) {
-//       if (search.includes(input[i])) {
-//         result.push(input[i]);
-//       }
-//     }
-//     return result;
-//   }
-//   console.log(match(['🍌', '🥝', '🍇'], ['🍌', '🍓', '🍇', '🍓']));
-
-
-// const arr1 = ['🍌', '🥝', '🍇'];
-// const arr2 = ['🍌', '🍓', '🍇', '🍓'];
-// const result = [];
-// arr1.forEach((ee) => {
-//     arr2.forEach((el) => {
-//         if(el.includes(ee)) {
-//             result.push(el);
-//         }
-//         return result;
-//     });
-// });
-
-// console.log(result);
-// console.clear();
-// arr2.forEach((el) => {
-//     arr1.flatMap((a) => {
-//         if(el.includes(a)) {
-//             result.push(el);
-//         }
-//         return result;
-//     });
-// });
-
-// console.log(result);
-
-
-// const nums = [3,16,5,25,4,34,21];
-// const comp = nums.filter((n) => 5 < n);
-// const hub = comp.reduce((sum, value) => {
-//     sum += value
-//     return sum;
-// });
-// console.log(hub/4);
+changeColor(prop);
